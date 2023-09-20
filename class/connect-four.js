8d967fb4948f7f25567ac1531fadeba728cba429
Oliver <oliverwang008@gmail.com>
2023-09-20 19:21:14 +1000

    code commit

--- class/connect-four.js
+++ class/connect-four.js
@@ -20,17 +20,38 @@ class ConnectFour {
     Screen.initialize(6, 7);
     Screen.setGridlines(true);
 
-    // Replace this with real commands
-    Screen.addCommand('t', 'test command (remove)', ConnectFour.testCommand);
+    Screen.addCommand('up', 'cursor up', this.cursor.up.bind(this.cursor));
+    Screen.addCommand('down', 'cursor down', this.cursor.down.bind(this.cursor));
+    Screen.addCommand('left', 'cursor left', this.cursor.left.bind(this.cursor));
+    Screen.addCommand('right', 'cursor right', this.cursor.right.bind(this.cursor));
+    Screen.addCommand('return', 'place move', this.placeMove.bind(this));
 
     this.cursor.setBackgroundColor();
     Screen.render();
   }
 
-  // Remove this
-  static testCommand() {
-    console.log("TEST COMMAND");
-  }
+  placeMove() {
+    let r = this.cursor.row;
+    let c = this.cursor.col;
+    if (this.grid[r][c] === ' ') {
+      if (this.playerTurn === 'O') {
+        this.grid[r][c] = 'O';
+        this.playerTurn = 'X';
+        Screen.setGrid(r, c, 'O');
+        Screen.render();
+      } else {
+        this.grid[r][c] = 'X';
+        this.playerTurn = 'O'
+        Screen.setGrid(r, c, 'X');
+        Screen.render();
+      }
+    }
+    let winner = ConnectFour.checkWin(this.grid);
+    if (winner) {
+      ConnectFour.endGame(winner);
+    }
+
+  };
 
   static checkWin(grid) {
 
@@ -38,6 +59,60 @@ class ConnectFour {
     // Return 'O' if player O wins
     // Return 'T' if the game is a tie
     // Return false if the game has not ended
+    for (let r = 0; r < 6; r++) {
+      for (let c = 0; c < 4; c++) {
+        if (grid[r][c] === 'X' && grid[r][c + 1] === 'X' && grid[r][c + 2] === 'X' && grid[r][c + 3] === 'X') {
+          return 'X';
+        }
+        if (grid[r][c] === 'O' && grid[r][c + 1] === 'O' && grid[r][c + 2] === 'O' && grid[r][c + 3] === 'O') {
+          return 'O';
+        }
+      }
+    }
+    for (let c = 0; c < 7; c++) {
+      for (let r = 0; r < 3; r++) {
+        if (grid[r][c] === 'X' && grid[r + 1][c] === 'X' && grid[r + 2][c] === 'X' && grid[r + 3][c] === 'X') {
+          return 'X';
+        }
+        if (grid[r][c] === 'O' && grid[r + 1][c] === 'O' && grid[r + 2][c] === 'O' && grid[r + 3][c] === 'O') {
+          return 'O';
+        }
+      }
+    }
+    for (let r = 0; r < 3; r++) {
+      for (let c = 0; c < 4; c++) {
+        if (grid[r][c] === 'X' && grid[r + 1][c + 1] === 'X' && grid[r + 2][c + 2] === 'X' && grid[r + 3][c + 3] === 'X') {
+          return 'X';
+        }
+        if (grid[r][c] === 'O' && grid[r + 1][c + 1] === 'O' && grid[r + 2][c + 2] === 'O' && grid[r + 3][c + 3] === 'O') {
+          return 'O';
+        }
+      }
+    }
+    for (let r = 0; r < 3; r++) {
+      for (let c = 3; c < 7; c++) {
+        if (grid[r][c] === 'X' && grid[r + 1][c - 1] === 'X' && grid[r + 2][c - 2] === 'X' && grid[r + 3][c - 3] === 'X') {
+          return 'X';
+        }
+        if (grid[r][c] === 'O' && grid[r + 1][c - 1] === 'O' && grid[r + 2][c - 2] === 'O' && grid[r + 3][c - 3] === 'O') {
+          return 'O';
+        }
+      }
+    }
+    
+    let tie = true;
+    for (let r = 0; r < 6; r++) {
+      for (let c = 0; c < 7; c++) {
+        if (grid[r][c] === ' ') {
+          tie = false;
+        }
+      }
+    }
+    if (tie) {
+      return 'T';
+    } else {
+      return false;
+    }
 
   }
 
--- class/cursor.js
+++ class/cursor.js
@@ -16,18 +16,48 @@ class Cursor {
 
   resetBackgroundColor() {
     Screen.setBackgroundColor(this.row, this.col, this.gridColor);
+    Screen.render();
   }
 
   setBackgroundColor() {
     Screen.setBackgroundColor(this.row, this.col, this.cursorColor);
+    Screen.render();
+  }
+
+  up () {
+    // Move cursor up
+    this.resetBackgroundColor();
+    if (this.row > 0) {
+      this.row -= 1;
+    }
+    this.setBackgroundColor();
+  }
+
+  down () {
+    // Move cursor down
+    this.resetBackgroundColor();
+    if (this.row < this.numRows - 1) {
+      this.row += 1;
+    }
+    this.setBackgroundColor();
   }
 
   left() {
     // Move cursor left
+    this.resetBackgroundColor();
+    if (this.col > 0) {
+      this.col -= 1;
+    }
+    this.setBackgroundColor();
   }
 
   right() {
     // Move cursor right
+    this.resetBackgroundColor();
+    if (this.col < this.numCols - 1) {
+      this.col += 1;
+    }
+    this.setBackgroundColor();
   }
 
 }
